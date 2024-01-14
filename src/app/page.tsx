7fde9b9668eed7dc22f393bc7ff43242c8f95682
akimoto-junya <akimoto14301@gmail.com>
2024-01-14 23:50:15 +0900

    feature(settings): husky test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 export default function Home() {
+  const test = "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>frontend</div>
-      <div>test</div>
+      <div>{test}</div>
     </main>
   )
 }
