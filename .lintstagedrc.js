d83854e3cd4ce66efc88235db17d8d9ddeef5c8e
akimoto-junya <akimoto14301@gmail.com>
2024-01-15 00:46:40 +0900

    feature(settings): husky test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,7 @@ const path = require('path');
 const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`;
 
+const buildFormatCommand = (filenames) => `prettier --write ${filenames.join(' ')}`;
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'prettier --write'],
+  './**/*.{js,jsx,ts,tsx}': [buildEslintCommand, buildFormatCommand],
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   const test = {
-    "test": "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest", "test2": "testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest"
-  }
+    test: 'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest',
+    test2: 'testtesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttest',
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>frontend</div>
       <div>{test.test}</div>
-
     </main>
-  )
+  );
 }
